5c6e2bdb1ac38f666c4db9ff19259aec15d54041
AbramAanderud <abramashaanderud@gmail.com>
2025-12-09 22:59:14 -0800

    "Having saving bug for saving generated images and uploads to DB"

--- create_tables.py
+++ create_tables.py
@@ -1,13 +1,13 @@
-# create_tables.py
 from app.db import Base, engine
-from app import models_db  # noqa: F401  # needed so models register with Base
+from app import models_db  
 
 
 def main():
     print("Creating tables in the database...")
+    print("Using DB:", engine.url)
     Base.metadata.create_all(bind=engine)
     print("Done.")
-
+ 
 
 if __name__ == "__main__":
     main()
